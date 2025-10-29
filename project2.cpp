de93c2ae651dceec699f4279bfe852bf3ddbe5f5
dyeaman05 <danielyeaman05@gmail.com>
2025-10-29 02:06:09 -0500

    finally got everything working, time for autograder

--- project2.cpp
+++ project2.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 // Structure to represent a process control block
 struct PCB {
@@ -12,23 +13,23 @@ struct PCB {
     int last_run_time;
     int remaining_quantum;
     int turnaround_time;
+    int waiting_time;
 };
 
-void print_process_time(PCB pcb, int start_time, int end_time) {
-        std::cout << "Time " << start_time << "-" << end_time << ": " << pcb.id 
-        << " (Priority " << pcb.priority << ")" << std::endl;
+void print_process_time(PCB pcb, int current_time) {
+    std::cout << current_time << ": " << pcb.id << " (Priority " << pcb.priority << ")" << std::endl;
 }
+
 
 //function that simulates the required scheduling process using preemptive priority and round robin
 void scheduler_simulator(std::vector<PCB> processes, int time_quantum) {
     int total_CPU_time = 0;     //stores total CPU time
     int CPU_working_time = 0;   //stores time that CPU is actually working on a process
-    int start_time = 0;         //stores start time for particular process
-    int end_time = 0;           //stores end time for particular process
-    
+    int current_time = 0;         //stores current CPU time
     PCB idle;                   //idle CPU process
     idle.id = "Idle";
     idle.priority = 0;
+    idle.remaining_time = 0;
 
     std::vector<PCB> sorted_pcbs = processes;   //create sorted_pcbs vector from given processes argument
 
@@ -51,35 +52,100 @@ void scheduler_simulator(std::vector<PCB> processes, int time_quantum) {
             if(i > sorted_pcbs.size()-1) i = 0;
         }
 
+        std::cout << "Time " << current_time << "-";
 
         //runs idle CPU process if process i is selected before arrival time
-        if(start_time < sorted_pcbs[i].arrival_time) {
-            for(int j = 0; j < (sorted_pcbs[i].arrival_time - start_time); j++) {
-                end_time++;
+        if(current_time < sorted_pcbs[i].arrival_time) {
+            while(current_time < sorted_pcbs[i].arrival_time) {
+                current_time++;
                 total_CPU_time++;
+                if(current_time >= sorted_pcbs[i].arrival_time) {
+                    print_process_time(idle, current_time);
+                    break;
+                }
             }
-            print_process_time(idle, start_time, end_time);
-            start_time = end_time;
+            continue;
         }
 
-        //runs selected process until completion or interruption
-        for(int j = 0; j < sorted_pcbs[i].burst_time; j++) {
-            sorted_pcbs[i].remaining_time--;
-            CPU_working_time--;
-            end_time++;
-
-            //interrupts active process to move to higher priority process
-            if(sorted_pcbs[i+1].arrival_time == end_time && sorted_pcbs[i+1].priority > sorted_pcbs[i].priority) {
-                break;
+        //if current and next process have equal priority, current process will execute for at most time_quantum units of time, then move to next process in round robin
+        if(sorted_pcbs[i].priority == sorted_pcbs[i+1].priority) {
+            for(int j = 0; j < time_quantum; j++) {
+                if (sorted_pcbs[i].remaining_time > 0) {
+                    sorted_pcbs[i].remaining_time--;
+                    CPU_working_time--;
+                    current_time++;
+                }
             }
-
+            print_process_time(sorted_pcbs[i], current_time);
+            if(sorted_pcbs[i].remaining_time == 0) {
+                sorted_pcbs[i].turnaround_time = current_time - sorted_pcbs[i].arrival_time;
+                sorted_pcbs[i].waiting_time = sorted_pcbs[i].turnaround_time - sorted_pcbs[i].burst_time;
+            }  
+            i++;    //move to next process for round robin
         }
-        //prints process time information
-        print_process_time(sorted_pcbs[i], start_time, end_time);
-        start_time = end_time;
-        i++;
-        if(i > sorted_pcbs.size()-1) i = 0;
+        //if current and previous process have equal priority, current process will execute for at most time_quantum units of time before either switching back to previous process in round robin,
+        //or moving forward to next process with higher priority
+        else if(sorted_pcbs[i].priority == sorted_pcbs[i-1].priority) {
+            for(int j = 0; j < time_quantum; j++) {
+                if (sorted_pcbs[i+1].arrival_time == current_time && sorted_pcbs[i+1].priority > sorted_pcbs[i].priority) {
+                    break;
+                }
+                if (sorted_pcbs[i].remaining_time > 0) {
+                    sorted_pcbs[i].remaining_time--;
+                    CPU_working_time--;
+                    current_time++;
+                }
+            }
+            print_process_time(sorted_pcbs[i], current_time);
+            if(sorted_pcbs[i].remaining_time == 0) {
+                sorted_pcbs[i].turnaround_time = current_time - sorted_pcbs[i].arrival_time;
+                sorted_pcbs[i].waiting_time = sorted_pcbs[i].turnaround_time - sorted_pcbs[i].burst_time;
+            }
+            if (sorted_pcbs[i+1].arrival_time == current_time && sorted_pcbs[i+1].priority > sorted_pcbs[i].priority) i++;
+            else i--;
+        }
+        //process executes as normal until finishing time burst
+        else {
+            while(sorted_pcbs[i].remaining_time > 0 ) {
+                if (sorted_pcbs[i].remaining_time > 0) {
+                    sorted_pcbs[i].remaining_time--;
+                    CPU_working_time--;
+                    current_time++;
+                }
+
+                if((sorted_pcbs[i+1].arrival_time == current_time && sorted_pcbs[i+1].priority > sorted_pcbs[i].priority) || sorted_pcbs[i].remaining_time == 0) {
+                    print_process_time(sorted_pcbs[i], current_time);
+                    //calculate turnaround and burst time if process has finished execution
+                    if(sorted_pcbs[i].remaining_time == 0) {
+                        sorted_pcbs[i].turnaround_time = current_time - sorted_pcbs[i].arrival_time;
+                        sorted_pcbs[i].waiting_time = sorted_pcbs[i].turnaround_time - sorted_pcbs[i].burst_time;
+                    }
+                    if(sorted_pcbs[i+1].arrival_time == current_time && sorted_pcbs[i+1].priority > sorted_pcbs[i].priority) i++;
+                    else i = 0;
+                    break;
+                }
+            }
+        }
+
+
+
     }
+
+    std::cout << "Turnaround Time" << std::endl;
+    for(PCB pcb : sorted_pcbs) {
+        std::cout << pcb.id << " = " << pcb.turnaround_time << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "Waiting Time" << std::endl;
+    for(PCB pcb : sorted_pcbs) {
+        std::cout << pcb.id << " = " << pcb.waiting_time << std::endl;
+    }
+
+
+
+
 };
 
 
